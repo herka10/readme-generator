639132799e604298e0a65dc042c39024e8a60736
herka10 <kaonou.her10@gmail.com>
2022-12-01 13:10:48 -0600

    added more inputs and writeFile to md vs html

---
+++ generateReadMe.js
@@ -0,0 +1,58 @@
+const generateReadMe = (data) => {
+
+    const {
+        title, 
+        what, 
+        why,
+        installation, 
+        usage, 
+        license,
+        contribution, 
+        test,
+        username,
+        email,
+    } = data   
+    
+    return `
+        ## ${title}
+        
+        # Description
+        Provide a short description explaining what your project is and why you created it. 
+        
+        ${what}
+        
+        ${why}
+
+        # Table of Contents (*** Can't get this to work & doesn't look like an actual md file - why?)
+        - [Installation](#Installation Instructions)
+        - [Usage](#Usage Information)
+        - [License](#License)
+        - [Contributing](#Contributing)
+        - [Tests](#Tests)
+        - [Questions](#Questions)
+
+
+        # Installation Instructions
+        What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+        
+        ${installation}
+
+        # Usage Information    
+        ${usage}
+
+        # License
+        ${license}
+
+        # Contributing
+        ${contribution}
+
+        # Tests
+        ${test}
+
+        # Questions
+        My GitHub Profile: ${username}
+        If there are additional questions, please reach me at ${email}
+    `
+}
+
+module.exports = generateReadMe
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-// fs is a Node standard library package for reading and writing files
+const generate = require("./generateReadMe")
 const fs = require('fs');
+
+// fs is a Node standard library package for reading and writing files
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -9,8 +11,8 @@ const questions = [];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     // fs.writeFile('log.txt', process.argv[2], (err) =>
-        //   err ? console.error(err) : console.log('Success!')
-        // );
+    //     err ? console.error(err) : console.log('Success!')
+    //     );
 }
 
 // TODO: Create a function to initialize app
@@ -25,8 +27,13 @@ function init() {
             },
             {
                 type: 'input',
-                message: 'What is the description of this ReadMe?',
-                name: 'description',
+                message: 'Provide a short description explaining what your project is.',
+                name: 'what',
+            },
+            {
+                type: 'input',
+                message: 'Provide a short description explaining why you created this project.',
+                name: 'why',
             },
             {
                 type: 'input',
@@ -39,6 +46,12 @@ function init() {
                 name: 'usage',
             },
             {
+                type: 'list',
+                message: 'Which of the following licenses describes your situation?',
+                choices: ['Need to work in a community - Apache License 2.0', 'Simple and permissive - MIT License', 'Care about sharing improvements - GNU General Public License v3.0'],
+                name: 'license',
+            },
+            {
                 type: 'input',
                 message: 'What are the contribution guidelines of this ReadMe?',
                 name: 'contribution',
@@ -48,10 +61,28 @@ function init() {
                 message: 'What are the test instructions of this ReadMe?',
                 name: 'test',
             },
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'username',
+            },
+            {
+                type: 'input',
+                message: 'Please enter an email address for contact purposes',
+                name: 'email',
+            },
+            
         ])
         .then((answers) => {
             // Use user feedback for... whatever!!
             // writeToFile(fileName, data)
+            const html = generate(answers)
+            console.log(html)
+
+            fs.writeFile('./readme2.md', html, error => {
+                if (error) throw error
+                console.log('ReadMe saved!')
+            })
         })
         .catch((error) => {
             if (error.isTtyError) {
@@ -64,3 +95,5 @@ function init() {
 
 // Function call to initialize app
 init();
+
+// *** HOW DO I RUN THIS INTO AN MD DOCUMENT VS HTML??? UGH. 
